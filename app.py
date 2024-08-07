70dcadee8a6fff580440fdc0a67910425a04fc54
MarkBish24 <markbish24@gmail.com>
2024-08-06 18:05:44 -0700

    Uploading first half of assignment

---
+++ app.py
@@ -0,0 +1,44 @@
+# Import the dependencies.
+%matplotlib inline
+from matplotlib import style
+style.use('fivethirtyeight')
+import matplotlib.pyplot as plt
+
+import numpy as np
+import pandas as pd
+import datetime as dt
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save references to each table
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
